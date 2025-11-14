1eeb7b80582c2d74c8f9f7be25732fbd07a53b7f
SwiftDust <mart.zielman@kpnmail.nl>
2025-11-14 13:36:06 +0100

    Add coyote time and proper ground logic

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -6,11 +6,11 @@ class_name Player extends CharacterBody2D
 @export var STEP_DISTANCE = 250.0
 @export var JUMP_VELOCITY = -600.0
 @export var MAX_JUMPS = 2
+# Coyote time is the time the user has to react if they fall off the platform
+@export var COYOTE_TIME_WINDOW = 0.1
 
-enum States {IDLE, BURSTING, JUMPING, FALLING}
-var state = States.IDLE
-
-var count := 0
+var jump_count := 0
+var time_in_air := 0.0
 var direction_sign := 0
 var target_distance := 0.0
 
@@ -18,16 +18,19 @@ var target_distance := 0.0
 func _physics_process(delta: float) -> void:	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		time_in_air += delta
 	else:
-		count = 0
+		time_in_air = 0
+		jump_count = 0
 		
-	if  Input.is_action_just_pressed("jump") and count < MAX_JUMPS:
-		count += 1
+	#if Input.is_action_just_pressed("jump") and (jump_count == MAX_JUMPS - 1 or (is_on_floor() or time_in_air < COYOTE_TIME_WINDOW)):
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or time_in_air < COYOTE_TIME_WINDOW or jump_count < MAX_JUMPS):
+		jump_count += 1
 		velocity.y = JUMP_VELOCITY
-		
+	
 	var direction := Input.get_axis("move_left", "move_right")
 	direction_sign = sign(direction)
-
+	
 	if Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
 		velocity.x = direction_sign * SPEED 
 		target_distance = STEP_DISTANCE
