b4f2dc659b14104c9759592e8f2ebbbc1f5c68cf
SwiftDust <mart.zielman@kpnmail.nl>
2025-11-17 22:04:33 +0100

    Add bouncing asteroids

--- Entities/Asteroid/asteroid.gd
+++ Entities/Asteroid/asteroid.gd
@@ -1,10 +1,23 @@
 class_name Asteroid extends RigidBody2D
 
-@export var ground: CollisionShape2D
+
+@export var velocity: Vector2 
+
+@onready var freeing_timer = $FreeingTimer
 
 
-func _on_body_entered(body: Node) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var bounce = move_and_collide(velocity * delta)
+	if bounce:
+		velocity = velocity.bounce(bounce.get_normal()) 
+
+
+func _on_freeing_timer_timeout() -> void:
+	queue_free()
+
+func _on_body_entered(body: Node) -> void:	
+	if body is StaticBody2D:
+		freeing_timer.start()
 	
 	# TODO: if body is area2d, then bounce, wait few seconds and queue_free()
 	# if it is player, then player should take live
--- World/game_manager.gd
+++ World/game_manager.gd
@@ -4,12 +4,9 @@ class_name GameManager extends Node
 @export var asteroid_scene: PackedScene
 @onready var asteroid_timer = $AsteroidTimer
 
-var lives := 0
-
 
 func _ready() -> void:
 	asteroid_timer.start()
-
 
 func _on_asteroid_timer_timeout() -> void:
 	var asteroid = asteroid_scene.instantiate()
@@ -18,5 +15,13 @@ func _on_asteroid_timer_timeout() -> void:
 	asteroid_spawn_location.progress_ratio = randf()
 	
 	asteroid.position = asteroid_spawn_location.position
+	
+	# quick note to myself in case I forget: 
+	# this means a full 360 degrees circle as 2*pi is 360 degrees
+	var random_angle = randf_range(0, 2 * PI)
+	var random_speed = randf_range(50, 150) 
+	
+	var direction = Vector2(cos(random_angle), sin(random_angle)) 
+	asteroid.velocity = direction * random_speed 
 	
 	add_child(asteroid)
